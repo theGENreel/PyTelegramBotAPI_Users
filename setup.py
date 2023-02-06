37e8f9cfedb1d69710c73d611c240b59dad960d1
Skummer <thegenreel@gmail.com>
2023-02-06 12:27:14 +0300

    Add requirements.txt. Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
 
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+
 setup(
-    name="PyTelegramBotApi Users",
+    name='PyTelegramBotApi_Users',
     version='1.0',
-    packages=find_packages()
+    description='User access manager for async pyTelegramBotAPI',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='skummer',
+    author_email='thegenreel@gmail.com',
+    url='https://github.com/theGENreel/PyTelegramBotAPI_Users',
+    packages=find_packages(),
+    license='GPL3',
+    install_requires=['pytelegrambotapi', 'aiofiles']
 )
